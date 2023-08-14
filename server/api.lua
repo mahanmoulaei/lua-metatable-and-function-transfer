891c1fe02e8b01cdcf6d5a5f0f90db4e137d82bd
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-08-14 18:57:56 -0400

    refactor: separate internal file and api file
    Not tested

---
+++ server/api.lua
@@ -0,0 +1,163 @@
+local api                         = {}
+local internalApi                 = {}
+
+internalApi.math                  = math
+internalApi.debug                 = debug
+internalApi.table                 = table
+internalApi.string                = string
+internalApi.handlerResource       = "x-test"
+internalApi.currentResource       = GetCurrentResourceName()
+internalApi.loadResourceFile      = LoadResourceFile
+internalApi.saveResourceFile      = SaveResourceFile
+internalApi.refreshHandlerExports = function()
+    internalApi.exportsMetadata = exports[internalApi.handlerResource]
+    return internalApi.exportsMetadata
+end
+
+do internalApi.refreshHandlerExports() end
+
+---Splits a string content into a table based on the specified separator
+---@param inputString string
+---@param separator? string
+---@return string[]
+local function splitString(inputString, separator)
+    local tbl, tblCount = {}, 0
+
+    if type(separator) ~= "string" then separator = "%s" end
+
+    for str in internalApi.string.gmatch(inputString, "([^" .. separator .. "]+)") do
+        tblCount += 1
+        tbl[tblCount] = str
+    end
+
+    return tbl
+end
+
+---Reloads the handler resource's exports metadata and caches it
+---@param resource string
+local function onResourceStart(resource)
+    return resource == internalApi.handlerResource and internalApi.refreshHandlerExports()
+end
+
+AddEventHandler("onResourceStart", onResourceStart)
+AddEventHandler("onServerResourceStart", onResourceStart)
+
+---Writes the passed function's definition, parameters, and body into a file for usage in external resources (**removes the function reference**)
+---@param func function
+---@return string?
+function api.writeFunction(func)
+    if type(func) ~= "function" then return error("The passed parameter is not a function type!") end
+
+    local info = internalApi.debug.getinfo(func, "Sn")
+    local functionResource = internalApi.currentResource
+
+    local sourcePath = info.source:sub(3) -- Remove the '@@' at the beginning
+    local sourceFileContent = internalApi.loadResourceFile(functionResource, sourcePath:sub(#functionResource + 1))
+    local sourceFileContentTable = splitString(sourceFileContent, "\n")
+
+    local desiredFunctionContentTable, desiredFunctionContentTableCount = {}, 0
+
+    local firstLine = sourceFileContentTable[info.linedefined]
+    desiredFunctionContentTableCount += 1
+    desiredFunctionContentTable[desiredFunctionContentTableCount] = ("function%s"):format(firstLine:match(".*(%b())"))
+
+    for i = info.linedefined + 1, info.lastlinedefined do
+        desiredFunctionContentTableCount += 1
+        desiredFunctionContentTable[desiredFunctionContentTableCount] = sourceFileContentTable[i]
+    end
+
+    local desiredFunctionContent = internalApi.table.concat(desiredFunctionContentTable, "\n")
+    local key = internalApi.exportsMetadata:generateKey()
+
+    local fileName = ("func_%s"):format(key)
+    local fullFilename = ("%s.lua"):format(fileName)
+    local outputFileContents = internalApi.string.format([[
+return {
+    ["%s"] = %s
+}
+    ]], fileName, desiredFunctionContent)
+
+    internalApi.saveResourceFile(internalApi.handlerResource, ("exports/%s"):format(fullFilename), outputFileContents, -1)
+
+    return fullFilename
+end
+
+---Writes the passed metatable's metamethods into a file for usage in external resources
+---@param metatable table
+---@return string?
+function api.writeMetatable(metatable)
+    if type(metatable) ~= "table" then return error("The passed parameter is not a table type!") end
+
+    local serializedTable = "-- Metatable\n"
+
+    for key, value in pairs(metatable) do
+        if type(value) == "function" then
+            local fileName = api.writeFunction(value)
+            serializedTable = serializedTable .. internalApi.string.format("local __metamethod_%s = LoadResourceFile(\"x-test\", \"%s\")\n", key, ("exports/%s"):format(fileName))
+        end
+    end
+
+    serializedTable = serializedTable .. "return getmetatable(setmetatable({}, {\n"
+    for key in pairs(metatable) do
+        serializedTable = serializedTable .. internalApi.string.format("[\"%s\"] = __metamethod_%s,\n", key, key)
+    end
+    serializedTable = serializedTable .. "}))\n"
+
+    local key = internalApi.exportsMetadata:generateKey()
+
+    local fileName = ("meta_%s"):format(key)
+    local fullFilename = ("%s.lua"):format(fileName)
+
+    internalApi.saveResourceFile(internalApi.handlerResource, ("exports/%s"):format(fullFilename), serializedTable, -1)
+
+    return fullFilename
+end
+
+---Loads the metatable from the specified file
+---@param fileName string
+---@return table?
+function api.loadMetatable(fileName)
+    local metatable = {}
+    local filePath = ("exports/%s"):format(fileName)
+    local chunk = internalApi.loadResourceFile(internalApi.handlerResource, filePath)
+
+    if not chunk then return error(("file %s doesn't exist!"):format(filePath)) end
+
+    local fn, err = load(chunk, ("@@%s/%s"):format(internalApi.handlerResource, filePath))
+
+    if fn and not err then
+        local savedTable = fn()
+
+        for key, value in pairs(savedTable) do
+            -- print(key, value)
+            if key:find("__") then
+                local _type = type(value)
+
+                if _type == "string" then
+                    local _fn, _err = load(value)
+
+                    if _fn and not _err then
+                        local mt = _fn()
+                        local _, func = next(mt)
+                        metatable[key] = func
+                    end
+                elseif _type == "function" then
+                    metatable[key] = value
+                elseif _type == "table" then
+                    local mt = getmetatable(value)
+                    mt.__index = nil
+                    mt.__newindex = nil
+                    metatable[key] = setmetatable(value, mt)
+                end
+
+                if not metatable[key] then
+                    error("Could not import " .. key .. " from " .. fileName)
+                end
+            end
+        end
+    end
+
+    return metatable
+end
+
+return api
--- server/main.lua
+++ server/main.lua
@@ -1,54 +1,32 @@
-local function stringsplit(inputString, separator)
-    if not separator then
-        separator = "%s"
+local generatedKeys = {}
+local action = setmetatable({}, {
+    __newindex = function(self, index, value)
+        rawset(self, index, value)
+        return exports(index, value)
     end
-    local t, i = {}, 1
-    for str in string.gmatch(inputString, "([^" .. separator .. "]+)") do
-        t[i] = str
-        i = i + 1
-    end
-    return t
-end
+})
 
--- Defining a function to write the passed function's definition, parameters, and body into a file (**removes the function reference**)
-local function writeFunction(func)
-    local info = debug.getinfo(func, "Sn")
-    local functionResource = GetCurrentResourceName()
+local mathLibrary = math
+local stringLibrary = string
 
-    local sourcePath = info.source:sub(3) -- Remove the '@@' at the beginning
-    local sourceFileContent = LoadResourceFile(functionResource, sourcePath:sub(#functionResource + 1))
-    local sourceFileContentTable = stringsplit(sourceFileContent, "\n")
-
-    local desiredFunctionContentTable, desiredFunctionContentTableCount = {}, 0
-
-    local firstLine = sourceFileContentTable[info.linedefined]
-    desiredFunctionContentTableCount += 1
-    desiredFunctionContentTable[desiredFunctionContentTableCount] = ("function%s"):format(firstLine:match(".*(%b())"))
-
-    for i = info.linedefined + 1, info.lastlinedefined do
-        desiredFunctionContentTableCount += 1
-        desiredFunctionContentTable[desiredFunctionContentTableCount] = sourceFileContentTable[i]
-    end
-
-    local desiredFunctionContent = table.concat(desiredFunctionContentTable, "\n")
+---@return string
+function action:generateKey()
     local key = ""
 
     repeat
-        key = key .. string.char(math.random(65, 90))
-    until key and #key == 5
+        key = key .. stringLibrary.char(mathLibrary.random(65, 90))
+    until #key == 5
 
-    local fileName = ("func_%s.lua"):format(key)
-    local outputFileContents = string.format([[
-return {
-    ["%s"] = %s
-}
-    ]], fileName:sub(1, -5), desiredFunctionContent)
+    if generatedKeys[key] then
+        return self:generateKey()
+    end
 
-    SaveResourceFile(functionResource, ("exports/%s"):format(fileName), outputFileContents, -1) -- Use your environment's file saving function
+    generatedKeys[key] = true
 
-    return fileName
+    return key
 end
 
+--[[
 local function loadFunction(fileName)
     local filePath = ("exports/%s"):format(fileName)
     local chunk = LoadResourceFile(GetCurrentResourceName(), filePath)
@@ -62,94 +40,17 @@ local function loadFunction(fileName)
     error("Could not load function from " .. fileName)
 end
 exports("loadFunction", loadFunction)
+]]
 
 -- Defining the function to be passed
-local function sum(x, y)
-    return x + y
-end
+-- local function sum(x, y)
+--     return x + y
+-- end
 
 -- Calling the write function to save the passed function to a lua file
 -- do writeFunction(sum) end
 
 ----------------------------------------------------------------------------------------------------------------------------
-
--- Defining a function to write the passed metatable into a file
-local function writeMetatable(metatable)
-    local serializedTable = ""
-
-    serializedTable = serializedTable .. "-- Metamethods\n"
-    for key, value in pairs(metatable) do
-        if type(value) == "function" then
-            local fileName = writeFunction(value)
-            serializedTable = serializedTable .. string.format('local __metamethod_%s = LoadResourceFile("x-test", "%s")\n', key, ("exports/%s"):format(fileName))
-        end
-    end
-
-    serializedTable = serializedTable .. 'return getmetatable(setmetatable({}, {\n'
-    for key, _ in pairs(metatable) do
-        serializedTable = serializedTable .. string.format('["%s"] = __metamethod_%s,\n', key, key)
-    end
-    serializedTable = serializedTable .. string.format('["mahanIndex"] = "mahanValue",\n')
-    serializedTable = serializedTable .. '}))\n'
-
-    local key = ""
-
-    repeat
-        key = key .. string.char(math.random(65, 90))
-    until key and #key == 5
-
-    local fileName = ("meta_%s.lua"):format(key)
-
-    SaveResourceFile(GetCurrentResourceName(), ("exports/%s"):format(fileName), serializedTable, -1) -- Use your environment's file saving function
-
-    return fileName
-end
-
-local function loadMetatable(fileName)
-    local metatable = {}
-    local filePath = ("exports/%s"):format(fileName)
-    local chunk = LoadResourceFile(GetCurrentResourceName(), filePath)
-
-    if not chunk then return error(("file %s doesn't exist!"):format(filePath)) end
-
-    local fn, err = load(chunk, ('@@%s/%s'):format(GetCurrentResourceName(), filePath))
-
-    if fn and not err then
-        local savedTable = fn()
-
-        for key, value in pairs(savedTable) do
-            -- print(key, value)
-            if key:find("__") then
-                local _type = type(value)
-
-                if _type == "string" then
-                    local _fn, _err = load(value)
-
-                    if _fn and not _err then
-                        local mt = _fn()
-                        local _, func = next(mt)
-                        metatable[key] = func
-                    end
-                elseif _type == "function" then
-                    metatable[key] = value
-                elseif _type == "table" then
-                    local mt = getmetatable(value)
-                    mt.__index = nil
-                    mt.__newindex = nil
-                    metatable[key] = setmetatable(value, mt)
-                end
-
-                if not metatable[key] then
-                    error("Could not import " .. key .. " from " .. fileName)
-                end
-            end
-        end
-    end
-
-    -- print(ESX.DumpTable(metatable))
-
-    return metatable
-end
 
 local meta = {
     __index = function(_, index)
@@ -163,18 +64,16 @@ local meta = {
 }
 
 CreateThread(function()
-    local obj = setmetatable({}, meta)
-    local fileName = writeMetatable(getmetatable(obj))
+    local api = lib.require("server.api")
 
-    local meta2 = loadMetatable(fileName)
+    local obj = setmetatable({}, meta)
+    local fileName = api.writeMetatable(getmetatable(obj))
+
+    local meta2 = api.loadMetatable(fileName)
     local obj2 = setmetatable({}, meta2)
 
     print(obj.sayMyName)  -- prints Mahan
     print(obj2.sayMyName) -- prints Mahan
 
-    obj2.kaftar = "25M"
-
-    print(ESX.DumpTable(obj2))
-
-    -- loadMetatable("meta_LHNQX.lua")
+    obj2.kaftar = "25M"   -- prints the meta.__newindex's content
 end)
